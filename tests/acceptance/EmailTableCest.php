d7aba27cdb66f2f8a0843ab358177494e2050c02
James Ward <git@jamesward.io>
2019-07-23 22:27:21 +0100

    +Temporary minor refactoring on test

--- tests/acceptance/EmailTableCest.php
+++ tests/acceptance/EmailTableCest.php
@@ -5,15 +5,31 @@ class EmailTableCest
     private $validTo = 'test@test.com';
     private $validSubject = 'my subject';
 
-    public function testCanOpenNewMessageModal(AcceptanceTester $I)
+    public function _before(AcceptanceTester $I)
     {
         $I->maximizeWindow();
         $I->loginAsAdmin();
         $I->amOnAdminPage('admin.php?page=wp-mail-catcher');
+    }
 
-        $this->openNewMessageModal($I);
-        $this->sendValidMessage($I);
+    public function testCanOpenNewMessageModal(AcceptanceTester $I)
+    {
+        $I->click('[data-codeception="new-message-modal-btn"]');
+        $I->seeElement('[data-codeception="new-message-modal-body"]');
+    }
 
+    public function testCanSendValidMessage(AcceptanceTester $I)
+    {
+        $this->testCanOpenNewMessageModal($I);
+        $I->selectOption('[data-codeception="header-select"]', 'to');
+        $I->fillField('[data-codeception="header-input"]', $this->validTo);
+        $I->fillField('subject', $this->validSubject);
+        $I->click('button[type=submit]');
+    }
+
+    public function testBulkDeleteLogs(AcceptanceTester $I)
+    {
+        $this->testCanSendValidMessage($I);
         $I->see($this->validTo);
         $I->see($this->validSubject);
         $I->checkOption('#cb-select-all-1');
@@ -23,20 +39,6 @@ class EmailTableCest
         $I->dontSee($this->validSubject);
 
         $I->makeScreenshot();
-    }
-
-    private function openNewMessageModal(AcceptanceTester $I)
-    {
-        $I->click('[data-codeception="new-message-modal-btn"]');
-        $I->seeElement('[data-codeception="new-message-modal-body"]');
-    }
-
-    private function sendValidMessage(AcceptanceTester $I)
-    {
-        $I->selectOption('[data-codeception="header-select"]', 'to');
-        $I->fillField('[data-codeception="header-input"]', $this->validTo);
-        $I->fillField('subject', $this->validSubject);
-        $I->click('button[type=submit]');
     }
 }
 
